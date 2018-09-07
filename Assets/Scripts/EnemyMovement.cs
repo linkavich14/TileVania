160b68883e9feb9789726a2df3da184fb8b31068
linkavich14 <5630335+linkavich14@users.noreply.github.com>
2018-09-07 13:14:07 +1000

    Added constrains, player animations, enemies animations

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D enemyRigidBody;
+    
+
+	// Use this for initialization
+	void Start () {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (IsFacingRight()) {
+            enemyRigidBody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else {
+            enemyRigidBody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+
+        
+	}
+
+    private bool IsFacingRight() {
+        return this.gameObject.transform.localScale.x > 0;
+    }
+
+    private void flipEnemy() {
+        bool enemyHasHorizontalSpeed = Mathf.Abs(enemyRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (enemyHasHorizontalSpeed) {
+            this.gameObject.transform.localScale = new Vector2(Mathf.Sign(enemyRigidBody.velocity.x), 1f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        this.gameObject.transform.localScale = new Vector2(-Mathf.Sign(enemyRigidBody.velocity.x), 1f);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,29 +8,36 @@ public class Player : MonoBehaviour {
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
     [SerializeField] float climbSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(25f, 25f);
 
     bool isAlive = true;
 
     Rigidbody2D myRigidBody;
     Animator myAnimator;
-    Collider2D myCollider2D;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeet;
     float gravityScaleAtStart;
 
 	// Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCollider2D = GetComponent<Collider2D>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
         gravityScaleAtStart = myRigidBody.gravityScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (!isAlive) {
+            return;
+        }
+
         Run();
         ClimbLadder();
         Jump();
         FlipSprite();
-        
+        Die();
 	}
 
     void Run () {
@@ -42,7 +49,7 @@ public class Player : MonoBehaviour {
     }
 
     private void ClimbLadder() {
-        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Climbing"))) {
             myAnimator.SetBool("Climbing", false);
             myRigidBody.gravityScale = gravityScaleAtStart;
             return;
@@ -59,16 +66,22 @@ public class Player : MonoBehaviour {
 
     }
 
-
     private void Jump() {
-        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) {
             return;
         }
-
 
         if (CrossPlatformInputManager.GetButtonDown("Jump")) {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
             myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void Die() {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("Enemy", "Hazards"))) {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
         }
     }
 
